73306f54253b8d77e4285a5aca9f90ef37551812
mengyuwang <mengyu.wang.0124@gmail.com>
2021-11-18 14:41:09 +0800

    背包问题

--- dynamic_programming/knapsackproblem/main.go
+++ dynamic_programming/knapsackproblem/main.go
@@ -28,22 +28,19 @@ func main() {
 	}
 	for i := 1; i <= len(productSlice); i++ {
 		for j := 1; j <= maxWeight; j++ {
-			// 当前商品的的重量是否超过的当前总重量
+			// 默认值不包含当前商品
+			dp[i][j] = dp[i-1][j]
+			selectItem[i][j] = selectItem[i-1][j] // 沿用不包含我的选项组合
 			if weightSlice[i-1] > j {
-				//就只能不包含自己. 只能取上一行的最大值. 就是当前列的上一行
-				dp[i][j] = dp[i-1][j]
-				selectItem[i][j] = selectItem[i-1][j]
+				// 当前商品重量超过了可容纳的上限, 就只能不容纳了
 				continue
 			}
-			// 当前商品的重量没有超过当前总重量,.
-			// 判断包含和不包含当前商品哪个价值大
-			notInclude := dp[i-1][j]                                 // 不包含我, 上一行最大值
+			// 计算包含当前商品的价值
 			include := dp[i-1][j-weightSlice[i-1]] + priceSlice[i-1] // 包含我:  我的价值+剩余空间的最大价值
-			dp[i][j] = dp[i-1][j]                                    //  默认选择不包含我
-			selectItem[i][j] = selectItem[i-1][j]
-			if include > notInclude { // 当包含我更好,则选用包含我
+			if include > dp[i-1][j] {                                // 当包含我比不包含我价值要高
 				fmt.Printf("包含了我: %s\n", productSlice[i-1])
-				dp[i][j] = include
+				dp[i][j] = include // 刷新当前单元格最高价值
+				// 将包含商品设置成不包含我时的列表加上当前商品
 				newMap := selectItem[i-1][j-weightSlice[i-1]]
 				newMap[productSlice[i-1]] = 1
 				selectItem[i][j] = newMap
