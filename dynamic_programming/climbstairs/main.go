67ecf4640b49f6d1835a25dd296bde68633881ea
mengyuwang <mengyu.wang.0124@gmail.com>
2022-06-22 15:33:23 +0800

    climb stairs

---
+++ dynamic_programming/climbstairs/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+/**
+  题目：假设你正在爬楼梯。需要 n 阶你才能到达楼顶。每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？注意：给定 n 是一个正整数。
+*/
+func main() {
+	res := climbStairs(6)
+	fmt.Println(res)
+}
+
+func climbStairs(n int) int {
+	if n == 1 {
+		return 1
+	}
+	dp := make([]int, n+1)
+	dp[1] = 1
+	dp[2] = 2
+	for i := 3; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
+}
