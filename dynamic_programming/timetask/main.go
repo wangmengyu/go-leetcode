42218c46400fc018a9cb5e931040bc2bf8aa9022
mengyuwang <mengyu.wang.0124@gmail.com>
2022-06-24 10:53:17 +0800

    opt path

--- dynamic_programming/timetask/main.go
+++ dynamic_programming/timetask/main.go
@@ -6,38 +6,41 @@ type TaskItem struct {
 	Start int
 	End   int
 	Value int
+	Path  map[int]int
 }
 
 func main() {
+
 	taskList := []TaskItem{
 		{
-			1, 4, 5,
+			1, 4, 5, nil,
 		},
 		{
-			3, 5, 1,
+			3, 5, 1, nil,
 		},
 		{
-			0, 6, 8,
+			0, 6, 8, nil,
 		},
 		{
-			4, 7, 4,
+			4, 7, 4, nil,
 		},
 		{
-			3, 8, 6,
+			3, 8, 6, nil,
 		},
 		{
-			5, 9, 3,
+			5, 9, 3, nil,
 		},
 		{
-			6, 10, 2,
+			6, 10, 2, nil,
 		},
 		{
-			8, 11, 4,
+			8, 11, 4, nil,
 		},
 	}
 
 	res := GetOpt(7, taskList)
 	fmt.Println(res)
+	fmt.Println(taskList[7].Path)
 }
 
 func Prev(n int, taskList []TaskItem) int {
@@ -71,16 +74,36 @@ func GetOpt(n int, taskList []TaskItem) int {
 		// 有上一部
 		excludeVal = GetOpt(n-1, taskList)
 	} else {
-		// 没上一部,直接返回包含当前值的公式
+		// 没上一部,直接返回包含当前值的公式 n + prev, 排除excluede:  n-1
 		fmt.Println("[use include n]", n)
+		//resMap[n] = n
+		// 选择当前N + PREV的路径
+		taskList[n].Path = make(map[int]int)
+		if prev >= 0 {
+			for k, _ := range taskList[prev].Path {
+				taskList[n].Path[k] = k
+			}
+		}
+		taskList[n].Path[n] = n
+
 		return includeVal
 	}
 
 	if includeVal > excludeVal {
 		fmt.Println("[use include n]", n)
+		//resMap[n] = n
+
+		taskList[n].Path = make(map[int]int)
+		if prev >= 0 {
+			for k, _ := range taskList[prev].Path {
+				taskList[n].Path[k] = k
+			}
+		}
+		taskList[n].Path[n] = n
+
 		return includeVal
 	}
-
+	taskList[n].Path = taskList[n-1].Path
 	fmt.Println("[use n-1]", n-1)
 	return excludeVal
 }
